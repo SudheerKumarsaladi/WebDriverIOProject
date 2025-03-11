facee2e1144e34194db6e77dfd289d9970a431f8
Sudheer <sudheerkumarsaladi4203@gmail.com>
2025-03-12 00:45:38 +0530

    third commit

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -6,7 +6,7 @@ import allureReporter from '@wdio/allure-reporter'
 describe('My Login application', () => {
 
      before(async()=>{
-        //Open the URL for testing
+        //Open the URL
         await LoginPage.open()
      })
         
