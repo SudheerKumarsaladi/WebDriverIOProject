32361cbb316eb05f8155f26e9ecd75cc9bc5d788
Sudheer <sudheerkumarsaladi4203@gmail.com>
2025-03-11 23:53:29 +0530

    newcode

---
+++ test/specs/test.e2e.js
@@ -0,0 +1,27 @@
+import { expect } from '@wdio/globals'
+import LoginPage from '../pageobjects/login.page.js'
+import SecurePage from '../pageobjects/secure.page.js'
+import allureReporter from '@wdio/allure-reporter'
+
+describe('My Login application', () => {
+
+     before(async()=>{
+        await LoginPage.open()
+     })
+        
+    it('should login with valid credentials', async () => {
+       
+        allureReporter.addFeature("launch th url successfully")
+        allureReporter.addSeverity('critical')
+        await LoginPage.login('tomsmith', 'SuperSecretPassword!')
+        allureReporter.addFeature("logged in successfully")
+        allureReporter.addSeverity('critical')
+
+
+        await expect(SecurePage.flashAlert).toBeExisting()
+        await expect(SecurePage.flashAlert).toHaveText(
+        await expect.stringContaining('You logged into a secure area!'))
+        await expect(SecurePage.flashAlert).toMatchElementSnapshot('flashAlert')
+    })
+})
+
