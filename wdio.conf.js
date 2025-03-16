512f98dab0d5ff0612227958b45e8eeeae54507e
Sudheer <sudheerkumarsaladi4203@gmail.com>
2025-03-16 13:11:45 +0530

    latestcode

--- wdio.conf.js
+++ wdio.conf.js
@@ -52,7 +52,10 @@ export const config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
+        'goog:chromeOptions':{
+            args: ['--headlesss', '--disable-gpu','--disable-dev-shn-usage']
+        },
     }],
 
     //
